979ffd1474c59c190aa5799dd84eb8f14007e8bf
Pooyan R <pooyan.rmz@gmail.com>
2023-02-02 09:21:14 -0600

    Update retrieve_clevr.sh

--- scripts/retrieve_clevr.sh
+++ scripts/retrieve_clevr.sh
@@ -4,12 +4,12 @@ DATA_PATH=your_data/clevr_change/data
 python -m torch.distributed.launch --nproc_per_node=2 main_task_retrieval.py \
 --do_train \
 --num_thread_reader=4 \
---epochs=12 \
+--epochs=15 \
 --batch_size=128 \
 --n_display=50 \
 --data_path ${DATA_PATH} \
 --features_path ${DATA_PATH} \
---output_dir ckpts/ckpt_clevr_retrieval \
+--output_dir ckpts/ckpt_clevr_caption_eval \
 --lr 1e-4 \
 --max_words 32 \
 --batch_size_val 128 \
@@ -17,5 +17,5 @@ python -m torch.distributed.launch --nproc_per_node=2 main_task_retrieval.py \
 --coef_lr 1e-3 \
 --freeze_layer_num 0 \
 --linear_patch 2d \
---pretrained_clip_name ViT-B/32
---init_model your_pretrained_model_path
+--pretrained_clip_name ViT-B/32 \
+--init_model ckpts/trained/pytorch_model.bin.clevr
